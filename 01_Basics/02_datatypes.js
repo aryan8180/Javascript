b645805972052c77b4880aa45fcf4a840f536613
Aryan Sharma <86112346+aryan8180@users.noreply.github.com>
2024-02-11 05:39:51 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(2+3) // we are using nodejs, not browser
+
+console.log( 3 
+    + 
+    3 )  // Code readibility should be high
+console.log("Aryan")
+
+let name = "aryan"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stand alone alue
+// undefined => declared but not assigned anything
+// symbol => unique
+
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
