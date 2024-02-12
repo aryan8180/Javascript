8056040207c103047c2d0a8e2c756d0be9a715cb
Aryan Sharma <86112346+aryan8180@users.noreply.github.com>
2024-02-12 04:54:15 +0000

    conversion and operation

--- 01_Basics/03_conversionoperation.js
+++ 01_Basics/03_conversionoperation.js
@@ -23,3 +23,18 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 
 console.log(typeof stringNumber)
+
+// Operations
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+let str1 = "Hello"
+let str2 = " Aryan"
+
+str3 = str1 + str2 
+console.log(str3)
+
+console.log("1"+2)
+console.log(1 + "2")
