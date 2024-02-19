b8e767b74a9f49b3910009f352194656fc559219
Aryan Sharma <86112346+aryan8180@users.noreply.github.com>
2024-02-19 12:00:19 +0000

    Numbers and Maths

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,27 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(typeof score);
+// console.log(typeof balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1))
+
+const otherNumber = 123.866
+// console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//--------------------------- Maths--------------------------- 
+// console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.round(4.3));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(7.2));
+// console.log(Math.floor(7.9));
+// console.log(Math.min(4,3,6,44,1,66));
+// console.log(Math.max(4,66,7,123,4,65));
+
+console.log(Math.random()*10)  // alwasy between 0 and 1
