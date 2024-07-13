6f0f9031485c3b255e447b0113a083821b01a540
Aryan Sharma <86112346+aryan8180@users.noreply.github.com>
2024-07-13 16:14:16 +0530

    Day 1 of 30 Days of JavaScript
    1. Know how to declare variable using var, let and const.
    2. Understand the different data types in JavaScript.
    3. Be able to use the typeof operator to identify the data type of variable.
    4. Understand the concept of variable reassignment and the immutability of const variables.

---
+++ day1.js
@@ -0,0 +1,76 @@
+// Tasks/Activities
+// Activity 1 Variable Declaration -> Task 1
+
+var num = 7;
+console.log(num);
+
+// Task 2
+
+let name = 'Aryan';
+console.log(name);
+
+// Activity 2 Constant Declaration -> Task 3
+
+const check = true;
+console.log(check);
+
+// Activity 3 Data Types -> Task 4
+
+let cnt = 7;
+let str = 'Happy';
+let stat = false;
+let capital = {"India":"New Delhi"};
+let chain = [7,10,18];
+
+console.log(typeof cnt);
+console.log(typeof str);
+console.log(typeof stat);
+console.log(typeof capital);
+console.log(typeof chain); // In JS, array are considered as objects
+
+// Activity 4 Reassigning Variables -> Task 5
+
+let high = 10;
+console.log("Initial value: " + high);
+high = 7;
+console.log("New value: " + high);
+
+// Activity 5 Understanding const -> Task 6
+
+const pi = 3.14;
+console.log("Value of PI:" + pi);
+//pi = 22/7;
+//console.log("Value of PI:" + pi); // TypeError: Assignment to constant variable.
+
+// Feature Request
+// 1. Variable Types Console Log
+
+let nameFirst = 'Aryan';
+let age = 21;
+let isStudent = true;
+let challenge = {"30DaysOfJavaScript":"By CodeWithChai"};
+let week = [1,2,3,4];
+
+console.log(nameFirst, typeof nameFirst);
+console.log(age, typeof age);
+console.log(isStudent, typeof isStudent);
+console.log(challenge, typeof challenge);
+console.log(week, typeof week);
+
+// 2. Reassignment Demo
+
+// let
+let favouriteNumber = 10;
+console.log(favouriteNumber);
+favouriteNumber = 7;
+console.log(favouriteNumber);
+
+// const
+const favouritePlayer = 'Cristiano Ronaldo'
+console.log("Favourite Player is: " + favouritePlayer);
+//favouritePlayer = 'Lionel Messi';
+//console.log("Favourite Player is: " + favouritePlayer); // TypeError: Assignment to constant variable.
+
+
+
+
