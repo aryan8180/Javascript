501e5ad56d4a2f8654189e39caab201dfcb13db9
Aryan Sharma <86112346+aryan8180@users.noreply.github.com>
2024-07-14 05:27:38 +0000

    Activity 2 Assignment Operators

--- Day 2/day2.js
+++ Day 2/day2.js
@@ -1,7 +1,7 @@
 // Day 2 Operators
 // Activity 1 Arithmetic Operations
 
-// Task 1
+// Task 1,2,3,4,5
 let num1 = 4;
 let num2 = 6;
 console.log(num1 + num2); // Add two numbers
@@ -9,3 +9,17 @@ console.log(num2 - num1); // Subtract two numbers
 console.log(num2 * num1); // Multiply two numbers
 console.log(num2 / num1); // Divide two numbers
 console.log(num2 % num1); // Remainder
+
+// Activity 2 Assignment Operators
+
+// Task 6
+let num3 = 5;
+num3 += 2;
+console.log(num3);
+
+// Task 7
+let num4 = 16;
+num4 -= 6;
+console.log(num4);
+
+// Activity 3 Comparison Operators
