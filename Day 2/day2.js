9811428fb2a76ff95e1462e79207a7262953e31c
Aryan Sharma <86112346+aryan8180@users.noreply.github.com>
2024-07-14 05:22:43 +0000

    Activity 1 Arithmetic Operations

---
+++ Day 2/day2.js
@@ -0,0 +1,11 @@
+// Day 2 Operators
+// Activity 1 Arithmetic Operations
+
+// Task 1
+let num1 = 4;
+let num2 = 6;
+console.log(num1 + num2); // Add two numbers
+console.log(num2 - num1); // Subtract two numbers
+console.log(num2 * num1); // Multiply two numbers
+console.log(num2 / num1); // Divide two numbers
+console.log(num2 % num1); // Remainder
