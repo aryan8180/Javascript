84a401f2949993171b9025ff84e57cbf61c5e0e3
Aryan Sharma <86112346+aryan8180@users.noreply.github.com>
2024-07-15 05:19:51 +0000

    Activity 3 Switch Case

--- Day 3/day3.js
+++ Day 3/day3.js
@@ -40,3 +40,56 @@ if(num3 > num4){
 }
 
 // Activity 3 Switch Case
+
+// Task 4
+
+let day = 7;
+switch (day) {
+    case 1:
+        console.log("Monday")
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid number");
+        break;
+}
+
+// Task 5
+let score = 80;
+switch (true) {
+    case score >= 90:
+        console.log("A");
+        break;
+    case score >= 80:
+        console.log("B");
+        break;
+    case score >= 60:
+        console.log("C");
+        break;
+    case score >= 40:
+        console.log("D");
+        break;
+    case score < 40:
+        console.log("F");
+        break;
+    default:
+        console.log("Invalid Score");
+        break;
+}
