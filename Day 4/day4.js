ad12821053e1ab60f0c2f4f99a87b611d3323991
Aryan Sharma <86112346+aryan8180@users.noreply.github.com>
2024-07-16 03:23:58 +0000

    Completed Activity 1 and 1 task of Activity 2

---
+++ Day 4/day4.js
@@ -0,0 +1,27 @@
+// Day 4 Loops
+// Activity 1 For Loop
+
+// Task 1
+
+let i;
+for(i = 1;i<=10;i++){
+    console.log(i);
+}
+
+// Task 2
+
+for(i = 1;i<=10;i++){
+    console.log(5*i);
+}
+
+// Activity 2 While Loop
+
+// Task 3
+
+let sum1 = 0;
+i = 1;
+while(i<=10){
+    sum1 += i;
+    i++;
+}
+console.log(`Sum of first 10 numbers is ${sum1}`);
