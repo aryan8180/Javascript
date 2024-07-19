32ca873a5999ad13ab232e5ad1e59bdcebc4fccd
Aryan Sharma <86112346+aryan8180@users.noreply.github.com>
2024-07-19 11:12:30 +0000

    Activity 5 Object Iteration

--- Day 7/day7.js
+++ Day 7/day7.js
@@ -29,7 +29,7 @@ console.log(book.info(2020))
 
 // Activity 3 Nested Objects
 
-// Task 5
+// Task 5,6
 
 const library = {
     name: 'Open Library',
@@ -40,3 +40,59 @@ const library = {
     ]
 };
 console.log(library);
+console.log(library.name);
+console.log(library.books);
+
+// Activity 4 The `this` keyword
+
+// Task 7
+
+const obj = {
+    name: 'Open Library',
+    showName: function() {
+        console.log(this.name);
+    }
+};
+obj.showName();
+
+// Activity 5 Object Iteration
+
+// Task 8
+
+const lib = {
+    name: 'Open Library',
+    books: {
+        book1: { title: 'Computer Networks' },
+        book2: { title: 'Operating Systems' },
+        book3: { title: 'Data Structures and Algorithms' }
+    }
+};
+
+for (const key in lib.books) {
+    if (lib.books.hasOwnProperty(key)) {
+        console.log(lib.books[key].title);
+    }
+}
+
+// Task 9
+
+const libr = {
+    name: 'Open Library',
+    books: {
+        book1: { title: 'Computer Networks' },
+        book2: { title: 'Operating Systems' },
+        book3: { title: 'Data Structures and Algorithms' }
+    }
+};
+
+Object.keys(libr.books).forEach(key => {
+    console.log(libr.books[key].title);
+});
+
+Object.values(libr.books).forEach(book => {
+    console.log(book.title);
+});
+
+
+
+
